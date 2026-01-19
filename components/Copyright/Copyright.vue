8782a1b4a039d57cb8899d99194ae8fdcfa611ed
Igor Stakic <stakic_igor@yahoo.com>
2026-01-19 23:12:26 +0100

    add copyright component setup

---
+++ components/Copyright/Copyright.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+    import dayjs from 'dayjs';
+</script>
+<template>
+    <section>
+        &copy; Sha Festival | {{ dayjs().year() }}
+    </section>
+</template>
--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+    import dayjs from 'dayjs'
+</script>   
+
 <template>
     <footer>
         Footer component
+        <Copyright />
     </footer>
 </template>
