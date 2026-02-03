f626af56bfa63b9108fa1a7b6df4d36c51275c93
Igor Stakic <stakic_igor@yahoo.com>
2026-02-03 21:42:04 +0100

    remove blog items

--- server/api/blog/index.get.ts
+++
@@ -1,9 +0,0 @@
-import { wordpressService } from "../services/wordpress.service";
-
-export default defineEventHandler(async (event) => {
-
-    const data = await wordpressService.getBlogPosts();
-
-    return data;
-    
-});
